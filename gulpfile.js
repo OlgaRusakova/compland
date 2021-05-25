3d9692bb78cf3f9e352b35da2bf7b20079e4ea4a
unknown <a.v.rusakou@gmail.com>
2021-05-26 00:35:38 +0300

    big commit

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ const autoprefixer  = require('gulp-autoprefixer');
 const gcmq = require('gulp-group-css-media-queries');
 const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
+const webpack = require('webpack-stream');
+const include = require('gulp-include');
 
 gulp.task("sass", function () {
   return gulp
@@ -35,9 +37,51 @@ gulp.task("browser-sync", function () {
   });
 });
 
+/* gulp.task("js", function () {
+  return gulp.src('./src/assets/js/index.js')
+    .pipe(include())
+      .on('error', console.log)
+      // 
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./src/assets/js'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+ */
+
+gulp.task("js", function () {
+  
+	return gulp
+  .src(['./src/assets/js/*.js', '!./src/assets/js/*.min.js'])
+		.pipe(webpack({
+			mode: 'production',
+			performance: { hints: false },
+			module: {
+				rules: [
+					{
+						test: /\.(js)$/,
+						exclude: /(node_modules)/,
+						loader: 'babel-loader',
+						query: {
+							presets: ['@babel/env'],
+							plugins: ['babel-plugin-root-import']
+						}
+					}
+				]
+			}
+		})).on('error', function handleError() {
+			this.emit('end')
+		})
+		.pipe(rename('index.min.js'))
+		.pipe(gulp.dest('./src/assets/js'))
+		.pipe(browserSync.reload({ stream: true }));
+});
+
+
+
 gulp.task("checkupdate", function () {
   gulp.watch("./src/assets/scss/**/*.scss", gulp.parallel("sass"));
   gulp.watch("./src/*.html").on('change', browserSync.reload);
+  gulp.watch("./src/assets/js/**/index.js", gulp.parallel("js"));
 });
 
 
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -44,3 +44,104 @@
     console.log(header);
 
 })();
+
+/* timer */
+function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((t / 1000) % 60);
+    const minutes = Math.floor((t / 1000 / 60) % 60);
+    const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+    };
+}
+
+function initializeClock(id, endtime) {
+    const clock = document.getElementById(id);
+    const daysSpan = clock.querySelector('.days');
+    const hoursSpan = clock.querySelector('.hours');
+    const minutesSpan = clock.querySelector('.minutes');
+    const secondsSpan = clock.querySelector('.seconds');
+
+    function updateClock() {
+const t = getTimeRemaining(endtime);
+
+daysSpan.innerHTML = t.days;
+hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+if (t.total <= 0) {
+        clearInterval(timeinterval);
+}
+    }
+
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+}
+const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
+initializeClock('countdown', deadline);
+
+import Swiper from 'swiper';
+import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+
+SwiperCore.use([Navigation, Pagination]);
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    slidesPerColumn: 2,  
+    slidesPerGroup: 2,
+    slidesPerColumnFill: 'row',
+    spaceBetween: 30, 
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+},
+breakpoints: {
+    // when window width is >= 320px
+    320: {
+        slidesPerView: 1.5,
+        slidesPerColumn: 2,  
+        slidesPerGroup: 2,
+        slidesPerColumnFill: 'row',
+        spaceBetween: 30, 
+    },
+
+    660: {
+        slidesPerView: 2,
+        slidesPerColumn: 2,  
+        slidesPerGroup: 2,
+        slidesPerColumnFill: 'row',
+        spaceBetween: 30, 
+    }
+}
+});
+
+const swiper2 = new Swiper(".teachersSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30, 
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+},
+breakpoints: {
+    // when window width is >= 320px
+    320: {
+        slidesPerView: 1.5
+    },
+
+    660: {
+        slidesPerView: 2,
+    },
+
+    990: {
+        slidesPerView: 3
+    },
+}
+});
+
