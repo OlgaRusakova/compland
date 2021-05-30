768b50230ebb104abcda130c4f92905de6500301
unknown <a.v.rusakou@gmail.com>
2021-05-30 19:11:36 +0300

    build

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,9 @@ const gcmq = require('gulp-group-css-media-queries');
 const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
 const webpack = require('webpack-stream');
-const include = require('gulp-include');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+
 
 gulp.task("sass", function () {
   return gulp
@@ -76,14 +78,40 @@ gulp.task("js", function () {
 		.pipe(browserSync.reload({ stream: true }));
 });
 
-
-
 gulp.task("checkupdate", function () {
   gulp.watch("./src/assets/scss/**/*.scss", gulp.parallel("sass"));
   gulp.watch("./src/*.html").on('change', browserSync.reload);
   gulp.watch("./src/assets/js/**/index.js", gulp.parallel("js"));
+  gulp.watch("./src/assets/img/**/*.*").on('change', browserSync.reload);
+});
+
+gulp.task('clean', async function () {
+  return del.sync('./dist');
 });
 
 
+gulp.task('prebuild', async function () {
+  let transferCss = gulp.src('./src/assets/css/*.css').pipe(gulp.dest('./dist/assets/css'));
+
+  let transferFonts = gulp.src('./src/assets/fonts/**/*').pipe(gulp.dest('./dist/assets/fonts'));
+
+  let transferImages = gulp.src('./src/assets/img/**/*').pipe(imagemin([
+    imagemin.gifsicle({interlaced: true}),
+    imagemin.mozjpeg({quality: 50, progressive: true}),
+    imagemin.optipng({optimizationLevel: 7}),
+    imagemin.svgo({
+        plugins: [
+            {removeViewBox: true},
+            {cleanupIDs: false}
+        ]
+    })
+])).pipe(gulp.dest('./dist/assets/img'));
+
+  let transferJs = gulp.src('./src/assets/js/*.min.js').pipe(gulp.dest('./dist/assets/js'));
+
+  let transferStructure = gulp.src(['./src/*', '!./src/assets/**/*']).pipe(gulp.dest('./dist'));
+});
 
 gulp.task("watch", gulp.parallel("sass", "checkupdate", "browser-sync",));
+
+gulp.task('build', gulp.series('clean', gulp.parallel( 'sass', 'js' ), 'prebuild'));
